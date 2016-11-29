de6e99e0c5531c72607e83067a43e742c5d92062
Alan McGinlay <amcginlay@gmail.com>
2016-11-29 00:05:33 +0000

    New Spring Starter Project (client app) with configclient, web and thymeleaf included.
    RateController links member variables (note, no model this time!) to config settings via the @Value annotation
    
    The rateview.html file is grafted from the downloadable java-microservices-spring-cloud-developing-services.zip file
    
    This project uses the config server.  Both will appear in the Boot Dashboard.  Make sure server is running first.

---
+++ springcloud-m2-configclient/src/main/java/com/cynaptec/springcloud/m2/configclient/SpringcloudM2ConfigclientApplication.java
@@ -0,0 +1,12 @@
+package com.cynaptec.springcloud.m2.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM2ConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM2ConfigclientApplication.class, args);
+	}
+}
