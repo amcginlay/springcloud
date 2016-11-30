7392720aca4e23a98bedd9bf3c9c01d77316b0fa
Alan McGinlay <amcginlay@gmail.com>
2016-11-30 18:46:38 +0000

    Add a new Maven dependency (spring-boot-actuator) to the client project and mark the RateController with the RefreshScope. Restart the client in the Boot Dashboard.
    Check the value of the rate at both:
    - localhost:8888/s1rates/default (the server web server)
    - http://localhost:8080/rate (the client web server)
    
    Now make a change to the rate in GitHub (https://github.com/amcginlay/springcloud-config/blob/master/station1/s1rates.properties)
    
    Go back and check the pages above.  Only the server has been refreshed (it goes to GitHub each time).
    
    Send a (basic) authenticated POST request to localhost:8080/refresh and revisit the client page which should now be in sync with GitHub.
    
    (In a web farm we'd want to distribute these refresh requests to each node - hopefully covered elsewhere!)

--- springcloud-m2-configclient/src/main/java/com/cynaptec/springcloud/m2/configclient/RateController.java
+++ springcloud-m2-configclient/src/main/java/com/cynaptec/springcloud/m2/configclient/RateController.java
@@ -3,11 +3,13 @@ package com.cynaptec.springcloud.m2.configclient;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 	
 	@Value("${rate}")
