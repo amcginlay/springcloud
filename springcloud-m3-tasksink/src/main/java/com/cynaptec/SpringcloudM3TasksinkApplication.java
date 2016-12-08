14d1cc25be390723530d56c5c80dd0bd4da4e05f
Alan McGinlay <amcginlay@gmail.com>
2016-12-08 15:43:38 +0000

    Create a new Spring Starter Project named springcloud-m3-tasksink with dependencies on Cloud Task and Stream Rabbit.
    The following additional dependency is added manually to the pom.xml
    
    		<!-- used for local testing, https://github.com/spring-cloud/spring-cloud-deployer -->
    		<!-- in production we'd use cloud foundry or kubernetes -->
    		<dependency>
    			<groupId>org.springframework.cloud</groupId>
    			<artifactId>spring-cloud-deployer-local</artifactId>
    			<version>1.0.3.RELEASE</version>
    		</dependency>

---
+++ springcloud-m3-tasksink/src/main/java/com/cynaptec/SpringcloudM3TasksinkApplication.java
@@ -0,0 +1,12 @@
+package com.cynaptec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM3TasksinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM3TasksinkApplication.class, args);
+	}
+}
