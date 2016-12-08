6ec48446e3b30ce1dbb0a1611b06d7fef581d7ea
Alan McGinlay <amcginlay@gmail.com>
2016-12-08 16:01:10 +0000

    Create new project named springcloud-m3-taskintake with dependencies on Cloud Task, Stream Rabbit and Web.

---
+++ springcloud-m3-taskintake/src/main/java/com/cynaptec/SpringcloudM3TaskintakeApplication.java
@@ -0,0 +1,12 @@
+package com.cynaptec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM3TaskintakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM3TaskintakeApplication.class, args);
+	}
+}
---
+++ springcloud-m3-taskintake/src/test/java/com/cynaptec/SpringcloudM3TaskintakeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cynaptec;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudM3TaskintakeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
