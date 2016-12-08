34fc9ba5281751bcef0410898ccf1c9e619ca38b
Alan McGinlay <amcginlay@gmail.com>
2016-12-08 11:54:21 +0000

    Proof of concept
    Mark our application class with @EnableTask and register a @Bean which implements CommandLIneRunner
    
    When we run the project from Boot Dashboard we can see our task's STDOUT result.
    
    It's all over in a flash ... these tasks are intentionally short-lived.

--- springcloud-m3-task/src/main/java/com/cynaptec/SpringcloudM3TaskApplication.java
+++ springcloud-m3-task/src/main/java/com/cynaptec/SpringcloudM3TaskApplication.java
@@ -1,12 +1,33 @@
 package com.cynaptec;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableTask
 public class SpringcloudM3TaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudM3TaskApplication.class, args);
+	}
+	
+	@Bean
+	public TollProcessingTask tollProcessingTask() {
+		return new TollProcessingTask();
+	}
+	
+	public class TollProcessingTask implements CommandLineRunner {
+
+		@Override
+		public void run(String... strings) throws Exception {
+			// parameters stationId, licence plate, timestamp
+			if (null != strings) {
+				System.out.println("Parameter length is " + strings.length);
+			}
+			
+		}
 	}
 }
