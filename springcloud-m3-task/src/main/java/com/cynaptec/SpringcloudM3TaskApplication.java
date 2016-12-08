2b1cc114f4758a686bc75111891c32495c006a79
Alan McGinlay <amcginlay@gmail.com>
2016-12-08 11:43:30 +0000

    Create new Spring Starter Project (currently empty) (TODO should the .mvn directory go in the .gitignore)

---
+++ springcloud-m3-task/src/main/java/com/cynaptec/SpringcloudM3TaskApplication.java
@@ -0,0 +1,12 @@
+package com.cynaptec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM3TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM3TaskApplication.class, args);
+	}
+}
